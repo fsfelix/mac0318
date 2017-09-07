4c9ab3a6f577bff65fbb105362dc1e3980ba0784
Lucas Helfstein <lucashelfstein@gmail.com>
2017-09-07 12:48:48 -0300

    Posição e metodos relacionados separados em outro arquivo. Falta testar no robô, usando locationTester

--- aula4/Position.java
+++ aula4/Position.java
@@ -1,45 +1,58 @@
+import lejos.nxt.NXTRegulatedMotor;
+
 public class Position {
 
-    public int x = 0;
-    public int y = 0;
-    public double teta = 0.0;
-    public double accum_delta_s = 0;
+    public double x                 = 0.0;
+    public double y                 = 0.0;
+    public double teta              = 0.0;
+    public double accum_delta_s     = 0.0;
+    public double raioDaRoda        = 2.8;
+	public double dist_entre_eixos  = 11.2;
 
-    public double raioDaRoda = 2.8;
-	public double dist = 11.2;  // distancia entre eixos
-
-    //constructor
+    // construtor (x, y, pose). pose em graus.
     public Position (int a, int b, double c) {
         x = a;
         y = b;
         teta = c;
     }
 
-    public void updatePosition() {
+    // normalizadora de angulos
+    public static double normalizeAngle(double a, double center) {
+        return a - (2 * Math.PI) * Math.floor((a + Math.PI - center) / (2 * Math.PI));
+    }
 
-		double teta0, x0, y0, xf, yf, tetaf;
+    // atualizar posicao, passando motores como parametro
+    public void updatePosition(NXTRegulatedMotor rb, NXTRegulatedMotor rc) {
+
+		double x0, y0, xf, yf, teta0, tetaf;
 		double delta_teta, delta_s, tachoB, tachoC, sum;
 
+        // posicao atual
     	x0 = this.x;
     	y0 = this.y;
     	teta0 = this.teta;
 
-    	teta0 = Math.toRadians(teta0);
-
+        // obter os tacometros que indicam todo o delta_s
     	tachoB = rb.getTachoCount();
     	tachoC = rc.getTachoCount();
 
+        // tudo em radianos
     	tachoB = Math.toRadians(tachoB);
     	tachoC = Math.toRadians(tachoC);
+        teta0 = Math.toRadians(teta0);
 
-    	delta_teta = (tachoB - tachoC) * (raioDaRoda) / dist;
+        // angulo = pose
+    	delta_teta = (tachoB - tachoC) * (raioDaRoda) / dist_entre_eixos;
 
 		sum = teta0 + delta_teta / 2;
 		sum = normalizeAngle(sum, Math.PI);
 
     	delta_s = (tachoB + tachoC) * raioDaRoda / 2;
-    	delta_s = delta_s - this.accum_delta_s;                                 // delta_s do ultimo periodo que o robo andou!
 
+        // delta_s do ultimo periodo que o robo andou!
+    	delta_s = delta_s - this.accum_delta_s;
+
+        // nova posicao, considerando apenas o delta_s do ultimo movimento
     	xf = x0 + delta_s * Math.cos(sum);
     	yf = y0 + delta_s * Math.sin(sum);
 
@@ -50,9 +63,22 @@ public class Position {
 		this.x = xf;
 		this.y = yf;
 		this.teta = tetaf;
-        this.accum_delta_s = (tachoB + tachoC) * raioDaRoda / 2;                // atualizar o delta_s acumulado
 
-		return pos;
+        // atualizar o delta_s acumulado
+        this.accum_delta_s = (tachoB + tachoC) * raioDaRoda / 2;
     }
 
+    // verifica se pertence a reta. epsilon e coeficiente angular hardcoded.
+    public boolean belongsToLine () {
+
+		double eps = 5;
+		double diff;
+
+		diff = Math.abs(this.y - this.x * 0.77); // verifica se pertence a reta
+
+		if (diff < eps)
+			return true;
+		else
+			return false;
+	}
 }
--- aula4/bug2.java
+++ aula4/bug2.java
@@ -17,113 +17,18 @@ public class bug2 {
     static NXTRegulatedMotor rb;
     static NXTRegulatedMotor rc;
 
-    public static double normalizeAngle(double a, double center) {
-		return a - (2 * Math.PI) * Math.floor((a + Math.PI - center) / (2*Math.PI));
-    }
-
-    private static double [] newPos(double [] lastPos) {
-
-    	double teta0, x0, y0;
-
-    	x0 = lastPos[0];
-    	y0 = lastPos[1];
-    	teta0 = lastPos[2];
-
-    	double xf, yf, tetaf, delta_teta, delta_s, raioDaRoda, tachoB, tachoC, sum;
-		double [] pos = new double[3];
-
-    	raioDaRoda = 2.8;
-
-    	teta0 = Math.toRadians(teta0);
-    	tachoB = rb.getTachoCount();
-    	tachoC = rc.getTachoCount();
-
-    	tachoB = Math.toRadians(tachoB);
-    	tachoC = Math.toRadians(tachoC);
-
-    	delta_teta = (tachoB - tachoC)*(raioDaRoda)/(11.2);
-
-		sum = teta0 + delta_teta/2;
-		sum = normalizeAngle(sum, Math.PI);
-
-    	delta_s = (tachoB + tachoC)*raioDaRoda/2;
-    	xf = x0 + delta_s*Math.cos(sum);
-    	yf = y0 + delta_s*Math.sin(sum);
-
-    	tetaf = teta0 + delta_teta;
-		tetaf = normalizeAngle(tetaf, Math.PI);
-
-		pos[0] = xf;
-		pos[1] = yf;
-		pos[2] = tetaf;
-
-		return pos;
-  	}
-
-    private static double [] supremeNewPos(Position lastPos, double lastDelta) {
-
-		double dist = 11.2;  // distancia entre eixos
-		double teta0, x0, y0, xf, yf, tetaf;
-		double delta_teta, delta_s, raioDaRoda, tachoB, tachoC, sum;
-		double [] pos = new double[3];
-
-    	x0 = lastPos.x;
-    	y0 = lastPos.y;
-    	teta0 = lastPos.teta;
-		raioDaRoda = 2.8;
-
-    	teta0 = Math.toRadians(teta0);
-    	tachoB = rb.getTachoCount();
-    	tachoC = rc.getTachoCount();
-
-    	tachoB = Math.toRadians(tachoB);
-    	tachoC = Math.toRadians(tachoC);
-
-    	delta_teta = (tachoB - tachoC) * (raioDaRoda) / dist;
-
-		sum = teta0 + delta_teta / 2;
-		sum = normalizeAngle(sum, Math.PI);
-
-    	delta_s = (tachoB + tachoC) * raioDaRoda / 2;
-    	delta_s = delta_s - lastDelta;
-
-    	xf = x0 + delta_s * Math.cos(sum);
-    	yf = y0 + delta_s * Math.sin(sum);
-
-    	tetaf = teta0 + delta_teta;
-		tetaf = normalizeAngle(tetaf, Math.PI);
-
-		pos[0] = xf;
-		pos[1] = yf;
-		pos[2] = tetaf;
-
-		lastDelta = (tachoB + tachoC) * raioDaRoda / 2;
-
-		return pos;
-    }
-
-    private static double updateDeltaS() {
-
-		// gets the FULL DELTA_S
-
-		double delta_s, tachoB, tachoC;
-    	double raioDaRoda = 2.8;
-
-    	tachoB = rb.getTachoCount();
-    	tachoC = rc.getTachoCount();
-
-    	tachoB = Math.toRadians(tachoB);
-    	tachoC = Math.toRadians(tachoC);
-
-    	delta_s = (tachoB + tachoC)*raioDaRoda/2;
-
-		return delta_s;
-    }
-
 	public static void unregulatedCPD() {
+
+		// dummy cpd. para ao apertar esc.
 
 		int u_linha, turn, erro, erroant;
 		double KP, KD;
+
+		erro 		= 0;
+		erroant 	= 0;
+		u_linha 	= 40; 	// 50
+		KP 			= 6;	// 6
+		KD 			= 3;	// 3
 
 		while(!Button.ESCAPE.isDown()) {
 			erro = 45 - light.getLightValue();
@@ -134,42 +39,29 @@ public class bug2 {
 		}
 	}
 
-	private static boolean isOnLine (Position pos) {
-
-		double eps = 5;
-		double diff;
-
-		diff = Math.abs(pos.y - pos.x * 0.77); // verifica se pertence a reta
-
-		if (diff < eps)
-			return true;
-		else
-			return false;
-	}
-
-    private static void printPos(double [] myPos) {
+    private static void printPos(Position pos) {
 		RConsole.println(" ");
 		RConsole.println(" ");
 		RConsole.println(" ");
-		RConsole.println("x: "+ myPos[0]);
-		RConsole.println("y: "+ myPos[1]);
-		RConsole.println("teta: "+ myPos[2]);
-		RConsole.println(" "+ myPos[2]);
+		RConsole.println("x: "+ pos.x);
+		RConsole.println("y: "+ pos.y);
+		RConsole.println("teta: "+ pos.teta);
+		RConsole.println(" ");
 		RConsole.println(" ");
 		RConsole.println(" ");
 		RConsole.println(" ");
     }
 
-	public static void locationTester(double [] actualPos, double actualDelta) {
+	public static void locationTester(Position pos) {
 
-		// initial position, print it
-		actualPos = supremeNewPos(actualPos, actualDelta);
-		actualDelta = updateDeltaS();
-		printPos(actualPos);
+		// Método para testar se Position e updatePosition estão funcionando!
 
-		// turn to some side
-		rb.rotateTo(180, true);
-		rc.rotate(-180);
+		// print initial position
+		printPos(pos);
+
+		// maybe turn to some side
+		// rb.rotateTo(180, true);
+		// rc.rotate(-180);
 
 		// go forward and stop after 5 seconds
 		rb.forward();
@@ -178,30 +70,24 @@ public class bug2 {
 		rb.stop(true);
 		rc.stop();
 
-		// final position print it
-		actualPos = supremeNewPos(actualPos, actualDelta);
-		actualDelta = updateDeltaS();
-		printPos(actualPos);
+		// print final position
+		pos.updatePosition(rb, rc);
+		printPos(pos);
 	}
 
     public static void main(String args[]) {
 
-		int u_linha, turn, erroant, erro;
-		double KP, KD, light_v, lastUsedDelta;
+		double light_v;
 		boolean boolb, boolc;
 		boolean FINISHED = false;
 
-		double [] pos = new double[3];
-		double [] actualPos = new double[3];
+		light 	= new LightSensor(SensorPort.S4);
+		mB 		= new NXTMotor(MotorPort.B);
+		mC 		= new NXTMotor(MotorPort.C);
+		rb 		= new NXTRegulatedMotor(MotorPort.B);
+		rc 		= new NXTRegulatedMotor(MotorPort.C);
 
 		RConsole.openAny(0);
-
-		light = new LightSensor(SensorPort.S4);
-		mB = new NXTMotor(MotorPort.B);
-		mC = new NXTMotor(MotorPort.C);
-		rb = new NXTRegulatedMotor(MotorPort.B);
-		rc = new NXTRegulatedMotor(MotorPort.C);
-
 		Button.waitForAnyPress();
 
 		// giro inicial
@@ -211,22 +97,11 @@ public class bug2 {
 		rb.resetTachoCount();
 		rc.resetTachoCount();
 
-		u_linha = 40; 	// 50
-		KP = 6; 		// 6
-		KD = 3; 		// 3
-
-		erroant = 0;
 		light_v = 0;
-
 		rb.setSpeed(200);
 		rc.setSpeed(200);
 
-		// position
-		actualPos[0]	= 0;
-		actualPos[1] 	= 0;
-		actualPos[2] 	= 37;
-		lastUsedDelta 	= 0.0;
-
+		// set initial position
 		Position ponto = new Position(0, 0, 37);
 
 		while (!FINISHED) {
