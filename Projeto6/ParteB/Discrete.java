be4e564ebeeb696829cce68541efc433a6495717
Felipe Felix <felipescript@gmail.com>
2017-10-13 23:31:57 -0300

    parte b seems legit

--- Projeto6/ParteB/Discrete.java
+++ Projeto6/ParteB/Discrete.java
@@ -245,11 +245,13 @@ public class Discrete {
         double h = distance(n, goal)/distance(new coord(0,0), new coord(M - 1, N - 1));
         double p = probMap[n.x()][n.y()];
         double c = costs[n.x()][n.y()]/(M * N);
+        double alpha = 1;
         System.out.println("h " + h);
         System.out.println("p " + p);
         System.out.println("c " + c);
         System.out.println("aqui dentro dando infinito? " + (c*p+h));
-        return c*p + h;
+        //return c*p + h;
+        return alpha*c + (1-alpha)*p + h;
     }
 
 
