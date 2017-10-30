86e18b5476ddf53bde13617492aecfd3eb0752ac
Lucas Helfstein <lucashelfstein@gmail.com>
2017-10-30 09:19:33 -0200

    Removi o metodo de pegar pontos mais distantes

--- Projeto7/EP/createMap.py
+++ Projeto7/EP/createMap.py
@@ -3,7 +3,7 @@ import sys
 
 class Line:
     def __init__(self, x0, y0, x1, y1):
-        # print("x0 = {}, y0 = {}, x1 = {}, y1 = {}".format(x0, y0, x1, y1))
+
         if (x0 == x1):
             self.m = math.inf
             self.x0 = x0
@@ -99,19 +99,6 @@ def init_extract(points, thres):
 
 def euclideanDist(p1, p2):
     return math.sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]))
-
-def mostDistantPoint(points):
-    x = 0
-    p0 = points[0]
-    points.remove(p0)
-
-    for point in points:
-        if euclideanDist(p0, point) > x:
-            x = euclideanDist(p0, point)
-            mostDistant = point
-
-    points.remove(mostDistant)
-    return p0, mostDistant
 
 def polarToEuclidian(pose, points, cont):
     euclidianPoints = []
