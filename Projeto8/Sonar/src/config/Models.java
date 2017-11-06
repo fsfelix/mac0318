6fb7cf264477e5c04bd67186c57eec2cce7e88de
Felipe Felix <felipesfelix@hotmail.com>
2017-11-06 16:35:42 -0200

    entrega ai helfs

--- Projeto8/Sonar/src/config/Models.java
+++ Projeto8/Sonar/src/config/Models.java
@@ -13,7 +13,7 @@ public class Models {
 		/**************** MODELO DO SONAR *******************/
 		Pose tmppose = new Pose(p.getX(), p.getY(), p.getHeading());
 		float mindist = Float.POSITIVE_INFINITY;
-    int teta2 = 24;
+    int teta2 = 17;
 		int cone = teta2*2;
 		for (int angulo=-cone/2; angulo <= cone/2; angulo++) {
 			tmppose.setHeading((float) (p.getHeading() - angulo + angle));
