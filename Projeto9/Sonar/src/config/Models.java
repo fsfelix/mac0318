cef4ff77254aee6e1f5a8826d2bee7e2f8a4aa50
Lucas Helfstein <lucashelfstein@gmail.com>
2017-11-23 11:36:14 -0200

    Arquivos nomeados corretamente, começo da extração de dados

---
+++ Projeto9/Sonar/src/config/Models.java
@@ -0,0 +1,26 @@
+package config;
+
+import lejos.robotics.mapping.LineMap;
+import lejos.robotics.navigation.Pose;
+
+public class Models {
+	private LineMap map;
+
+	public Models(LineMap map) {
+		this.map = map;
+	}
+
+	public double expectedSonarRead(Pose p, double angle) {
+		/**************** MODELO DO SONAR *******************/
+		Pose tmppose = new Pose(p.getX(), p.getY(), p.getHeading());
+		float mindist = Float.POSITIVE_INFINITY;
+		int cone = 0;	
+		for (int angulo=-cone/2; angulo <= cone/2; angulo++) {
+			tmppose.setHeading((float) (p.getHeading() - angulo + angle));
+			float dist = map.range(tmppose);
+			if (dist > 0 && dist < mindist) 
+				mindist = dist;
+		} 
+		return mindist;
+	}
+}
